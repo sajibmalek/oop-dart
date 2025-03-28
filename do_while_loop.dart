64dc4d5b4c4197728ffb9fdd7b314d9d84ccc5a4
sajibmalek <a.malek0317@gmail.com>
2025-03-28 22:38:35 +0600

    do_while loop in dart

---
+++ do_while_loop.dart
@@ -0,0 +1,17 @@
+/*
+do{
+    statement1;
+    statement2;
+    .
+    .
+    .
+    statementN;
+}while(condition);
+*/
+void main() {
+  int i = 0;
+  do {
+    print(i);
+    i++;
+  } while (i < 10);
+}
